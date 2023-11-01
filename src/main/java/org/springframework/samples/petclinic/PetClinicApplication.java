55a8b77677cb6c199ad3ca1f24a5cd62d2c6db7d
Cory <cory.jett@gmail.com>
2023-11-01 15:11:59 -0400

    Update PetClinicApplication.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -34,9 +34,9 @@ public class PetClinicApplication {
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
 
-	@Bean
-	public InMemoryHttpExchangeRepository createTraceRepository() {
-	  return new InMemoryHttpExchangeRepository();
-	}
+}
 
+@Bean
+public InMemoryHttpExchangeRepository createTraceRepository() {
+  return new InMemoryHttpExchangeRepository();
 }
